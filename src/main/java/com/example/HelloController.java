b827efbe3c39c464276657196e26ce3e897fe68f
rutujaaa007 <rutujashivpuje7@gmail.com>
2025-08-08 17:09:24 +0530

    Inital commit of my Spring Bot project

--- src/main/java/com/example/HelloController.java
+++ src/main/java/com/example/HelloController.java
@@ -1,14 +1,31 @@
 package com.example;
-
+ 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+ 
 @RestController
 public class HelloController {
-
-	@GetMapping("/")
-	public String Hello() {
-		return "Hello world, this is Rutuja";
-	}
-
+ 
+    // This method name violates naming conventions (should start with lowercase)
+    @GetMapping("/")
+    public String HELLO() {
+ 
+        // Hardcoded string, bad practice
+        String message = "Hie This is Rutuja";
+ 
+        // Unused variable
+        int unusedVariable = 100;
+ 
+        // Duplicate logic (dead code)
+        if (message.equals("Hello world")) {
+            System.out.println("Message is correct");
+        }
+ 
+        if (message.equals("Hello Rutuja")) {
+            System.out.println("Message is correct");
+        }
+ 
+        // Redundant return
+        return message;
+    }
 }
